b39ba6a6674dfd9e1129400f77880da36fcbea92
lhross1 <89688827+lhross1@users.noreply.github.com>
2024-02-02 15:40:10 -0300

    20220202

---
+++ SpringCarrosCRUD/src/main/java/com/springcrud/SpringCarrosCRUD/SpringCarrosCrudApplication.java
@@ -0,0 +1,13 @@
+package com.springcrud.SpringCarrosCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCarrosCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCarrosCrudApplication.class, args);
+	}
+
+}
